7b300a6e85df4a52f7b5a1a61869a19dea555323
rafaelbispodosantos <rafael_bispo23@hotmail.com>
2021-01-14 12:38:53 -0300

    hender
    hender funcionando

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import App from './main/app'
 
-ReactDom.render(<APP/>, document.getElementById('app'))
+ReactDom.render(<App/>, document.getElementById('app'))
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,7 +1,12 @@
+import '../common/template/dependencias'
 import React from 'react'
-export default props =>(
-    <div className='wrapper'>
-        <h1>App</h1>
 
+import Header from '../common/template/header'
+
+export default props => (
+    <div className='wrapper'>
+        <Header />
+        
+        
     </div>
 )
